a8c34580f68751b3f921f75b7fc2c4a35c56c495
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-14 00:22:52 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.8e72d942ec16abef0a44b1b032254846.js"
 );
 
 self.addEventListener('message', (event) => {
